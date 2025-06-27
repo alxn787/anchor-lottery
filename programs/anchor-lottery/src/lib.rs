95b879b77a0f3dcc1f73ac018af989ae849fdbe2
alxn787 <alenabraham787@gmail.com>
2025-06-27 23:21:53 +0530

    init config

--- programs/anchor-lottery/src/lib.rs
+++ programs/anchor-lottery/src/lib.rs
@@ -6,11 +6,50 @@ declare_id!("EuXtguR5S8nPqBMZNg3qMMCVjN53u4obRbwQL5zzB8xQ");
 pub mod anchor_lottery {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, start:u64, end:u64, price:u64) -> Result<()> {
+        ctx.accounts.token_lottery.bump = ctx.bumps.token_lottery;
+        ctx.accounts.token_lottery.start_time = start;
+        ctx.accounts.token_lottery.end_time = end;
+        ctx.accounts.token_lottery.ticket_price = price;
+        ctx.accounts.token_lottery.authority = ctx.accounts.payer.key();
+        ctx.accounts.token_lottery.randomness_account = Pubkey::default();
+        ctx.accounts.token_lottery.winner_chosen = false;
+        ctx.accounts.token_lottery.lottery_pot_amt = 0;
+        ctx.accounts.token_lottery.total_tickets = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + TokenLottery::INIT_SPACE,
+        seeds = [b"token_lottery".as_ref()],
+        bump
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct TokenLottery {
+    pub bump: u8,
+    pub winner: u64,
+    pub winner_chosen: bool,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub lottery_pot_amt: u64,
+    pub total_tickets: u64,
+    pub ticket_price: u64,
+    pub authority: Pubkey,
+    pub randomness_account: Pubkey,
+}
+
