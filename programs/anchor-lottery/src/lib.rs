f1fece19dfdf6891c2156fb3d832148cfbd40e29
alxn787 <alenabraham787@gmail.com>
2025-06-28 09:56:38 +0530

    init config

--- programs/anchor-lottery/src/lib.rs
+++ programs/anchor-lottery/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, metadata::Metadata, token_interface::{Mint, TokenAccount, TokenInterface}};
 
 declare_id!("EuXtguR5S8nPqBMZNg3qMMCVjN53u4obRbwQL5zzB8xQ");
 
@@ -47,7 +48,56 @@ pub struct InitializeLottery<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
 
-    pub sysyem_program: Program<'info, System>,
+
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = collection_mint,
+        mint::freeze_authority = collection_mint,
+        seeds = [b"collection_mint".as_ref()],
+        bump
+    )]
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+
+
+    #[account(
+        init,
+        payer = payer,
+        token::mint = collection_mint,
+        token::authority = collection_token_account,
+        seeds = [b"collection_associated_token".as_ref()],
+        bump
+    )]
+    pub collection_token_account : InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    ///CHECK:This account is checked by metadata program
+    pub metadata: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+            seeds = [b"metadata", 
+                token_metadata_program.key().as_ref(), 
+                collection_mint.key().as_ref(),
+                b"edition"
+            ],
+        bump,
+        seeds::program = token_metadata_program
+    )]
+    /// CHECK:This account is checked by metadata program
+    pub master_edition: UncheckedAccount<'info>,
+
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub associate_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info,TokenInterface>,
+    pub system_program: Program<'info, System>,
+
 
 }
 
